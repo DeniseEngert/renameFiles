4cc8f60716103188c1c9e66ab51ac6caef14d054
DeniseEngert <10726000+DeniseEngert@users.noreply.github.com>
2024-05-20 09:36:55 +0200

    small corrections

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 import re
 
-dir = "<abolute Path>"
+dir = "<absolute Path>"
 pattern = "<Pattern>"
 
 files = [f for f in os.listdir(dir) if re.match(pattern, f)]
 
 for x in files:
     y = x.replace(pattern, '')
-    print(y)
     os.rename(os.path.join(dir, x), os.path.join(dir, y))
